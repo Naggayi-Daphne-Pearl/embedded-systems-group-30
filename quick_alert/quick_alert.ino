5d5f36e61202df26ea7c55ab5f0244143eeac9fd
ANANDA <anandalewis29@gmail.com>
2024-07-25 12:00:19 +0300

    Deleted unnecessary lines of code and rearranged the code structure

--- quick_alert/quick_alert.ino
+++ quick_alert/quick_alert.ino
@@ -58,49 +58,42 @@ void GasSmokeLevel() {
 // }
 void loop() {
   int temperature = dhtSensor.readTemperature();
-  //readSms();
-  //delay(3000);
-  // sending sms to multiple contacts
-  int i;
-  //for(i=0;i<numContacts;++i){
-    //bool result = Sim800L.sendSms((char*)contacts[i], "Hello from Quick Alert System");
-    //Serial.print("SMS send result: ");
-    //Serial.println(result ? "Success" : "Failure");
-    //delay(200);
-    
- // }
-  Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.println(" °C");
+ 
   GasSmokeLevel();
-  Serial.println(sizeof(contacts));
-  Serial.println(sizeof(contacts[0]));
-  Serial.println(numContacts);
-
-  if (temperature > 28) {
-    Sim800L.callNumber("+256785796401");
-    // Sim800L.sendSms("+256702439337", "Hello from Quick Alert System");
-    delay(1000);
-    digitalWrite(redLed, HIGH);  // Turn on red LED
-    digitalWrite(buzzer, HIGH);
-    digitalWrite(greenLed, LOW);  // Turn off green LED
-
-    //Sim800L.callNumber("+256785796401");
-
-  }
-  //delay(3000);
-  else if (temperature < 28) {
-    digitalWrite(redLed, LOW);  // Turn off red LED
-    digitalWrite(greenLed, HIGH);
-    digitalWrite(buzzer, LOW);  // Turn on green LED
-
-  } else if (temperature != DHT11::ERROR_CHECKSUM && temperature != DHT11::ERROR_TIMEOUT) {
+  if (temperature != DHT11::ERROR_CHECKSUM && temperature != DHT11::ERROR_TIMEOUT) {
     Serial.print("Temperature: ");
     Serial.print(temperature);
     Serial.println(" °C");
-  } else {
+    delay(1000);  
+
+    if (temperature > 28) {
+      // sending sms to multiple contacts
+    int i;
+    for(i=0;i<numContacts;++i){
+      bool result = Sim800L.sendSms((char*)contacts[i], "Hello from Quick Alert System");
+      Serial.print("SMS send result: ");
+      Serial.println(result ? "Success" : "Failure");
+      delay(200);
+    }
+
+      Sim800L.callNumber("+256702439337");
+      digitalWrite(redLed, HIGH);  // Turn on red LED
+      digitalWrite(buzzer, HIGH);
+      digitalWrite(greenLed, LOW);  // Turn off green LED
+      //Sim800L.callNumber("+256785796401")
+    }
+  
+    //delay(3000);
+    else {
+      digitalWrite(redLed, LOW);  // Turn off red LED
+      digitalWrite(greenLed, HIGH);
+      digitalWrite(buzzer, LOW);  // Turn on green LED
+    } 
+  }
+
+   else {
     // Print error message based on the error code.
     Serial.println("Cannot Read Temperature"); 
   }
-  delay(5000);  // Adjust delay as needed for your application
+  
 }
